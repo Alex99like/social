673bdee4c191c649edece3f58d764b40c32dafc0
Alexander <aleksievisa@gmail.com>
2023-08-06 21:02:09 +0300

    feat: top-bar & left-bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <UserButton afterSignOutUrl='/' />
+      <h1 className={'head-text text-left'}>Home</h1>
     </main>
   )
 }
