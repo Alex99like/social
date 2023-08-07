d92e47b8f0b6d34555d31c47e0508ee523822afd
Alexander <aleksievisa@gmail.com>
2023-08-08 02:45:53 +0300

    feat: add activity page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -144,28 +144,28 @@ export async function fetchUsers({
   }
 }
 
-// export async function getActivity(userId: string) {
-//   try {
-//     connectToDB();
+export async function getActivity(userId: string) {
+  try {
+    connectToDB();
 
-//     const userThreads = await Thread.find({ author: userId });
+    const userThreads = await Thread.find({ author: userId });
 
-//     const childThreadIds = userThreads.reduce((acc, userThread) => {
-//       return acc.concat(userThread.children);
-//     }, []);
+    const childThreadIds = userThreads.reduce((acc, userThread) => {
+      return acc.concat(userThread.children);
+    }, []);
 
-//     const replies = await Thread.find({
-//       _id: { $in: childThreadIds },
-//       author: { $ne: userId },
-//     }).populate({
-//       path: "author",
-//       model: User,
-//       select: "name image _id",
-//     });
+    const replies = await Thread.find({
+      _id: { $in: childThreadIds },
+      author: { $ne: userId },
+    }).populate({
+      path: "author",
+      model: User,
+      select: "name image _id",
+    });
 
-//     return replies;
-//   } catch (error) {
-//     console.error("Error fetching replies: ", error);
-//     throw error;
-//   }
-// }
+    return replies;
+  } catch (error) {
+    console.error("Error fetching replies: ", error);
+    throw error;
+  }
+}
