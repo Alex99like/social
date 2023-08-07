314d106246ea3376c964722aecfde99b0221faaa
Alexander <aleksievisa@gmail.com>
2023-08-07 23:56:09 +0300

    feat: add search bar

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -93,56 +93,56 @@ export async function fetchUserPosts(userId: string) {
   }
 }
 
-// export async function fetchUsers({
-//   userId,
-//   searchString = "",
-//   pageNumber = 1,
-//   pageSize = 20,
-//   sortBy = "desc",
-// }: {
-//   userId: string;
-//   searchString?: string;
-//   pageNumber?: number;
-//   pageSize?: number;
-//   sortBy?: SortOrder;
-// }) {
-//   try {
-//     connectToDB();
+export async function fetchUsers({
+  userId,
+  searchString = "",
+  pageNumber = 1,
+  pageSize = 20,
+  sortBy = "desc",
+}: {
+  userId: string;
+  searchString?: string;
+  pageNumber?: number;
+  pageSize?: number;
+  sortBy?: SortOrder;
+}) {
+  try {
+    connectToDB();
 
-//     const skipAmount = (pageNumber - 1) * pageSize;
+    const skipAmount = (pageNumber - 1) * pageSize;
 
-//     const regex = new RegExp(searchString, "i");
+    const regex = new RegExp(searchString, "i");
 
-//     const query: FilterQuery<typeof User> = {
-//       id: { $ne: userId },
-//     };
+    const query: FilterQuery<typeof User> = {
+      id: { $ne: userId },
+    };
 
-//     if (searchString.trim() !== "") {
-//       query.$or = [
-//         { username: { $regex: regex } },
-//         { name: { $regex: regex } },
-//       ];
-//     }
+    if (searchString.trim() !== "") {
+      query.$or = [
+        { username: { $regex: regex } },
+        { name: { $regex: regex } },
+      ];
+    }
 
-//     const sortOptions = { createdAt: sortBy };
+    const sortOptions = { createdAt: sortBy };
 
-//     const usersQuery = User.find(query)
-//       .sort(sortOptions)
-//       .skip(skipAmount)
-//       .limit(pageSize);
+    const usersQuery = User.find(query)
+      .sort(sortOptions)
+      .skip(skipAmount)
+      .limit(pageSize);
 
-//     const totalUsersCount = await User.countDocuments(query);
+    const totalUsersCount = await User.countDocuments(query);
 
-//     const users = await usersQuery.exec();
+    const users = await usersQuery.exec();
 
-//     const isNext = totalUsersCount > skipAmount + users.length;
+    const isNext = totalUsersCount > skipAmount + users.length;
 
-//     return { users, isNext };
-//   } catch (error) {
-//     console.error("Error fetching users:", error);
-//     throw error;
-//   }
-// }
+    return { users, isNext };
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    throw error;
+  }
+}
 
 // export async function getActivity(userId: string) {
 //   try {
